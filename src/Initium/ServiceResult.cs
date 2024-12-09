d4e1fbdef0ae30f05766ec895fe063d992b81345
clint21.eth <me@clint21.dev>
2024-12-09 16:49:37 +0100

    Enhance ServiceResult with status code overloads.

--- src/Initium/ServiceResult.cs
+++ src/Initium/ServiceResult.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Initium.Exceptions;
 using Initium.Infrastructure;
@@ -7,19 +8,32 @@ namespace Initium;
 
 public class ServiceResult : BaseResult
 {
+	[JsonIgnore]
 	public bool Failed => !Success;
 	
-	public static ServiceResult Ok(string? message = null) => new()
+	public static ServiceResult Ok(string? message = null, HttpStatusCode statusCode = HttpStatusCode.OK) => new()
 	{
 		Success = true,
 		Message = message,
-		StatusCode = HttpStatusCode.OK
+		StatusCode = statusCode
+	};
+
+	public static ServiceResult Ok(HttpStatusCode statusCode) => new()
+	{
+		Success = true,
+		StatusCode = statusCode
 	};
 
 	public static ServiceResult Error(string? message = null, HttpStatusCode statusCode = HttpStatusCode.BadRequest) => new()
 	{
-		Success = false, 
+		Success = false,
 		Message = message,
+		StatusCode = statusCode
+	};
+
+	public static ServiceResult Error(HttpStatusCode statusCode) => new()
+	{
+		Success = false,
 		StatusCode = statusCode
 	};
 	
@@ -33,20 +47,36 @@ public class ServiceResult<TData> : ServiceResult
 {
 	public TData? Data { get; set; }
 
-	public static ServiceResult<TData> Ok(TData data, string? message = null) => new()
+	public static ServiceResult<TData> Ok(TData data, string? message = null, HttpStatusCode statusCode = HttpStatusCode.OK) => new()
 	{
 		Success = true,
+		Data = data,
 		Message = message,
-		StatusCode = HttpStatusCode.OK,
-		Data = data
+		StatusCode = statusCode
 	};
-	
-	public static ServiceResult<TData> Error(string? message = null, HttpStatusCode? statusCode = HttpStatusCode.BadRequest) => new()
+
+	public static ServiceResult<TData> Ok(TData data, HttpStatusCode statusCode) => new()
+	{
+		Success = true,
+		Data = data,
+		StatusCode = statusCode
+	};
+
+	public new static ServiceResult<TData> Error(string? message = null, HttpStatusCode statusCode = HttpStatusCode.BadRequest) => new()
 	{
 		Success = false,
-		StatusCode =  statusCode,
-		Message = message
+		Message = message,
+		StatusCode = statusCode
 	};
+
+	public new static ServiceResult<TData> Error(HttpStatusCode statusCode) => new()
+	{
+		Success = false,
+		StatusCode = statusCode
+	};
+	
+	public static implicit operator TData?(ServiceResult<TData> baseResult) => baseResult.Data;
+	public static implicit operator ServiceResult<TData>(TData? data) => new() { Data = data };
 	
 	public TData Unwrap() => Data ?? throw new ApiException(HttpStatusCode.InternalServerError, "Unwrapping failed, data is null.");
 }
