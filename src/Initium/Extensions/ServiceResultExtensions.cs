93e733071ab9a8b3e7a4f2c2498b14407eeb8878
clint21.eth <me@clint21.dev>
2024-12-09 18:06:07 +0100

    Refactor ServiceResultExtensions for clarity and default data.

--- src/Initium/Extensions/ServiceResultExtensions.cs
+++ src/Initium/Extensions/ServiceResultExtensions.cs
@@ -11,17 +11,17 @@ public static class ServiceResultExtensions
 	/// Converts a <see cref="ServiceResult"/> into a <see cref="ServiceResult{TData}"/> by attaching the specified data.
 	/// </summary>
 	/// <typeparam name="TData">The type of the data to attach to the result.</typeparam>
-	/// <param name="baseResult">The base <see cref="ServiceResult"/> to which the data will be added.</param>
+	/// <param name="serviceResult">The base <see cref="ServiceResult"/> to which the data will be added.</param>
 	/// <param name="data">The data to attach to the result.</param>
 	/// <returns>
 	/// A new <see cref="ServiceResult{TData}"/> instance containing the same status, message, and status code as the original result,
 	/// but enriched with the specified data.
 	/// </returns>
-	public static ServiceResult<TData> WithData<TData>(this ServiceResult baseResult, TData data) => new()
+	public static ServiceResult<TData> WithData<TData>(this ServiceResult serviceResult, TData? data = default) => new()
 	{
-		Success = baseResult.Success,
-		Message = baseResult.Message,
-		StatusCode = baseResult.StatusCode,
+		Success = serviceResult.Success,
+		Message = serviceResult.Message,
+		StatusCode = serviceResult.StatusCode,
 		Data = data
 	};
 }
