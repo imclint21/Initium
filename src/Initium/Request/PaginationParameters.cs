79f3e785fdb64cfb3e50afd304855569c4f75d44
clint21.eth <me@clint21.dev>
2025-01-08 17:07:48 +0100

    Increase to 30 items in the pagination parameters class.

--- src/Initium/Request/PaginationParameters.cs
+++ src/Initium/Request/PaginationParameters.cs
@@ -8,5 +8,5 @@ namespace Initium.Request;
 public class PaginationParameters
 {
 	public int Page { get; set; } = 1;
-	public int PageSize { get; set; } = 10;
+	public int PageSize { get; set; } = 30;
 }
