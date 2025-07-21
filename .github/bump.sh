bf69d9d65bf70fff9babab10589fd507dbee9172
clint21.eth ⚡️ <me@clint21.dev>
2025-07-21 21:50:24 +0200

    Fix auto bump

--- .github/bump.sh
+++ .github/bump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-current_build=$(grep -o "<Version>0.2.\(.*\)<\/Version>" ./src/Initium/Initium.csproj | sed 's/<Version>021.\(.*\)<\/Version>/\1/')
+current_build=$(grep -o "<Version>0.2.\(.*\)<\/Version>" ./src/Initium/Initium.csproj | sed 's/<Version>0.2.\(.*\)<\/Version>/\1/')
 new_build=$(printf "%d" $((10#$current_build + 1)))
-sed -i "s/<Version>0.2.$current_build<\/Version>/<Version>021.$new_build<\/Version>/" ./src/Initium/Initium.csproj
+sed -i "s/<Version>0.2.$current_build<\/Version>/<Version>0.2.$new_build<\/Version>/" ./src/Initium/Initium.csproj
 
 # Write the new build to the environment file
 echo "new_build=0.2.$new_build" >> $GITHUB_ENV
