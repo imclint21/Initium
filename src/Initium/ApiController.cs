6c9789765175862e9bc0e3a529c9f846304370e8
clint21.eth ⚡️ <me@clint21.dev>
2025-07-21 18:38:06 +0200

    Adding generic API controller that provides lazy access to a service of type TService.

--- src/Initium/ApiController.cs
+++ src/Initium/ApiController.cs
@@ -1,18 +1,27 @@
+using System.Net;
+using Initium.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Initium.Filters;
 using Initium.Infrastructure;
 
 namespace Initium;
 
+/// <summary>
+/// Base API controller providing global filters and routing.
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 [TypeFilter(typeof(ApiExceptionFilter))]
 [TypeFilter(typeof(ApiResponseFilter))]
 [TypeFilter(typeof(LoggingFilter))]
 [TypeFilter(typeof(ImplicitValidationFilter))]
-public class ApiController : BaseController;
+[TypeFilter(typeof(CustomHeaderFilter))]
+public abstract class ApiController : BaseController;
 
-public class ApiController<TService>(TService service) : ApiController where TService : class
+/// <summary>
+/// Generic API controller that provides lazy access to a service of type TService.
+/// </summary>
+public abstract class ApiController<TService> : ApiController where TService : BaseService
 {
-	public TService Service { get; } = service;
+	protected TService Service => (TService?)HttpContext.RequestServices.GetService(typeof(TService)) ?? throw new ApiException(HttpStatusCode.InternalServerError, $"Service of type `{typeof(TService).Name}` is not registered in the dependency injection container.");
 }
