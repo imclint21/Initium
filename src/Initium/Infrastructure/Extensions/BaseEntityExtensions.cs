5e104a537ce9ebc85fdb09529384e9fa9710b942
clint21.eth ⚡️ <me@clint21.dev>
2025-09-17 18:09:01 +0200

    Fix build error

--- src/Initium/Infrastructure/Extensions/BaseEntityExtensions.cs
+++ src/Initium/Infrastructure/Extensions/BaseEntityExtensions.cs
@@ -1,8 +1,8 @@
-using Initium.Domain.Entities;
-
-namespace Initium.Infrastructure.Extensions;
-
-internal static class BaseEntityExtensions
-{
-	public static void Touch<TKey>(this BaseEntity<TKey> entity) => entity.UpdatedAt = DateTimeOffset.UtcNow;
-}
+// using Initium.Domain.Entities;
+//
+// namespace Initium.Infrastructure.Extensions;
+//
+// internal static class BaseEntityExtensions
+// {
+// 	public static void Touch<TKey>(this BaseEntity<TKey> entity) => entity.UpdatedAt = DateTimeOffset.UtcNow;
+// }
